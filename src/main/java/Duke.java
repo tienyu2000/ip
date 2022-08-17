67bb140775d26e1cfeb892ebef35538ccf00cf73
tienyu2000 <tienyu2000@hotmail.com>
2022-08-17 20:14:48 +0800

    Level-1(updated)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,9 +4,9 @@ public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm nothing but an echo bot for now.\nWhat can I do for you, my highness?");
         Scanner in = new Scanner(System.in);
-        System.out.println("Your highness:");
 
         while (true) {
+            System.out.println("Your highness:");
             String input = in.nextLine();
             if (input.equals("bye")) {
                 System.out.println("Echoing: Bye. Hope to see you again soon!");
